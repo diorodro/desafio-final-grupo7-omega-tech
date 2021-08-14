2965b54b02b5633bacb9de8467103e7c065f931b
Vinícius Menezes <viniciusm_azevedo@hotmail.com>
2021-08-14 18:26:40 -0300

    Adding modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { UsersModule } from './users/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule, AuthenticationModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION_TIME: Joi.string().required(),
+        PORT: Joi.number(),
+      })
+    }),
+    AuthenticationModule,
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
