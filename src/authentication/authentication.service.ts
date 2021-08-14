a58f4c6c1433df5a826f27c8adbc3fbe447e305c
Vinícius Menezes <viniciusm_azevedo@hotmail.com>
2021-08-14 18:29:46 -0300

    Using JWT, fixing imports and adding LogOut

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,13 +1,19 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { UsersService } from 'src/users/services/users.service';
+import UsersService  from '../users/services/users.service';
 import RegisterDto from './dto/register.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import TokenPayload from './tokenPayload.interface';
 
+@Injectable()
 export class AuthenticationService {
   constructor(
-    private readonly usersService: UsersService
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService
   ) {}
- 
+
   public async register(registrationData: RegisterDto) {
     const hashedPassword = await bcrypt.hash(registrationData.password, 10);
     try {
@@ -24,25 +30,34 @@ export class AuthenticationService {
       throw new HttpException('Something went wrong', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
-  
-   public async getAuthenticatedUser(email: string, plainTextPassword: string) {
-  try {
-    const user = await this.usersService.getByEmail(email);
-    await this.verifyPassword(plainTextPassword, user.password);
-    user.password = undefined;
-    return user;
-  } catch (error) {
-    throw new HttpException('Wrong credentials provided', HttpStatus.BAD_REQUEST);
+
+  public getCookieWithJwtToken(userId: number) {
+    const payload: TokenPayload = { userId };
+    const token = this.jwtService.sign(payload);
+    return `Authentication=${token}; HttpOnly; Path=/; Max-Age=${this.configService.get('JWT_EXPIRATION_TIME')}`;
   }
-}
- 
-private async verifyPassword(plainTextPassword: string, hashedPassword: string) {
-  const isPasswordMatching = await bcrypt.compare(
-    plainTextPassword,
-    hashedPassword
-  );
-  if (!isPasswordMatching) {
-    throw new HttpException('Wrong credentials provided', HttpStatus.BAD_REQUEST);
+
+  public getCookieForLogOut() {
+    return `Authentication=; HttpOnly; Path=/; Max-Age=0`;
   }
-}
+
+  public async getAuthenticatedUser(email: string, plainTextPassword: string) {
+    try {
+      const user = await this.usersService.getByEmail(email);
+      await this.verifyPassword(plainTextPassword, user.password);
+      return user;
+    } catch (error) {
+      throw new HttpException('Wrong credentials provided', HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  private async verifyPassword(plainTextPassword: string, hashedPassword: string) {
+    const isPasswordMatching = await bcrypt.compare(
+      plainTextPassword,
+      hashedPassword
+    );
+    if (!isPasswordMatching) {
+      throw new HttpException('Wrong credentials provided', HttpStatus.BAD_REQUEST);
+    }
+  }
 }
