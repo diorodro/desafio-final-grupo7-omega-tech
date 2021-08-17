1c4a37d2efd0e4a170b07e90c3ca223112c15446
Vinícius Menezes <viniciusm_azevedo@hotmail.com>
2021-08-17 19:33:08 -0300

    Fixing the authModule

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -7,6 +7,9 @@ import { LocalStrategy } from './local.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './jwt.strategy';
+import { UsersService } from 'src/users/services/users.service';
+// import User from 'src/users/entities/user.entity';
+// import { Repository } from 'typeorm';
 
 @Module({
   imports: [
@@ -24,7 +27,11 @@ import { JwtStrategy } from './jwt.strategy';
       }),
     }),
   ],
-  providers: [AuthenticationService, LocalStrategy, JwtStrategy],
-  controllers: [AuthenticationController]
+  providers: [AuthenticationService, 
+  LocalStrategy, 
+  JwtStrategy, 
+  UsersService
+  ],
+  controllers: [AuthenticationController],
 })
 export class AuthenticationModule {}
