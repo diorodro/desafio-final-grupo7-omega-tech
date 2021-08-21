1e94fcca803c15777f562d9056027b286ba48083
Vinícius Menezes <viniciusm_azevedo@hotmail.com>
2021-08-20 22:09:52 -0300

    Removing imports

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -8,8 +8,6 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './jwt.strategy';
 import { UsersService } from 'src/users/services/users.service';
-// import User from 'src/users/entities/user.entity';
-// import { Repository } from 'typeorm';
 
 @Module({
   imports: [
