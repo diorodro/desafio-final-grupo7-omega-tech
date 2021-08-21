d70bc776ff431995e7fd7350c9b93b4ca69dec94
Vinícius Menezes <viniciusm_azevedo@hotmail.com>
2021-08-21 11:13:13 -0300

    Implementing Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Generate Propost example')
+    .setDescription('The Generate Proposals API description')
+    .setVersion('1.0')
+    .addTag('GenerateProps')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   await app.listen(3000);
